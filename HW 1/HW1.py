2f45a46d366a089036bb09e962331a70348d719e
Yoga096 <46023776+Yoga096@users.noreply.github.com>
2020-03-17 00:45:13 +0800

    Add files via upload

--- HW 1/HW1.py
+++ HW 1/HW1.py
@@ -1,54 +1,38 @@
-### 本金平均攤還試算
-
-### 輸入本金、期數及年利率
-
 print("本金(萬元) : ")
 c = int(input()) * 10000
-# 計算本金 * 萬
 
 print("期數(年) : ")
 n = int(input()) * 12 
-# 計算總期數
 
 print("年利率(%) : ") 
 r = float(input()) * 0.01 / 12 
-# 計算月利率
-
-result = [] # 試算結果清單
-cp = round(c / n) # 每期攤還本金 (四捨五入到個位)
-cic = 0 # 本金利息累計
 
 
+result = []
+cn = round(c / n)
+cic = 0 
+ic = 0
+ctp = c
 
-### 試算本金平均攤還
 
 for i in range(n-1) :
-# 計算 1 ~ 倒數第2期金額
 
-    result.append([i + 1, round(cp), round((c * r))])
-    # 在清單中加入期數、每期攤還本金、本期利息 (四捨五入到個位)
-    
-    cic += round(cp + (c * r))
-    # 計算本金利息累計 (四捨五入到個位)
+    inow = round(ctp * r)
+    cic += (cn + inow)
+    ic += inow
+    ctp -= cn
+    result.append([cn, inow, cic])
+   
 
-    c -= cp
-    # 計算剩餘本金
+inow = round(ctp * r)
+cic += (ctp + inow)
+ic += inow
+result.append([ctp, inow, cic])
 
-    result[i].append(round(cic))
-    # 在清單中加入本期為止本金利息累計 (四捨五入到個位)
-
-
-# 計算最後一期金額 (四捨五入到個位)
-result.append([n, round(c), round((c * r))])
-cic += round(c + (c * r))
-result[n - 1].append(round(cic))
-
-
-
-### 印出試算結果
     
 for j in range(n) :
-    print("第" + str(result[j][0]) + "期",
-          "本金(元) : " + str(result[j][1]), 
-          "利息(元) : " + str(result[j][2]),
-          "本金利息累計 : " + str(result[j][3]))
+    print("第" + str(j + 1) + "期",
+          "本金(元) : " + str(result[j][0]), 
+          "利息(元) : " + str(result[j][1]),
+          "本金利息累計 : " + str(result[j][2]))
+print("全部利息 : " + str(ic))
