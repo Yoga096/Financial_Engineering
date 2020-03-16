60e415040de043124928e23f610a7922540bc7b6
Yoga096 <46023776+Yoga096@users.noreply.github.com>
2020-03-17 01:17:16 +0800

    Add files via upload

--- HW 1/HW1.py
+++ HW 1/HW1.py
@@ -1,34 +1,34 @@
 print("本金(萬元) : ")
-c = int(input()) * 10000
+c = int(input()) * 10000  # c: 本金
 
 print("期數(年) : ")
-n = int(input()) * 12 
+n = int(input()) * 12   # n: 期數
 
 print("年利率(%) : ") 
-r = float(input()) * 0.01 / 12 
+r = float(input()) * 0.01 / 12  # r: 利率
 
 
-result = []
-cn = round(c / n)
-cic = 0 
-ic = 0
-ctp = c
+result = [] # result: 試算結果清單
+cn = round(c / n)   # cn: 每期攤還本金
+cic = 0  # cic: 本金利息累計
+ic = 0  # ic: 利息累計
+ctp = c  # ctp: 剩餘待還本金
 
 
-for i in range(n-1) :
+for i in range(n) :
 
-    inow = round(ctp * r)
+    inow = round(ctp * r)   # inow: 本期利息
     cic += (cn + inow)
     ic += inow
     ctp -= cn
     result.append([cn, inow, cic])
-   
+
 
 inow = round(ctp * r)
 cic += (ctp + inow)
 ic += inow
 result.append([ctp, inow, cic])
-
+   
     
 for j in range(n) :
     print("第" + str(j + 1) + "期",
