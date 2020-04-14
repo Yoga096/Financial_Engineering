0e4f85a66f334e934a7cf8ba528dacf7ed298df7
Yoga096 <46023776+Yoga096@users.noreply.github.com>
2020-04-14 16:30:43 +0800

    Update HW 3.py

--- HW 3/HW 3.py
+++ HW 3/HW 3.py
@@ -13,14 +13,18 @@ R = round(math.exp(r), 1)
 print("無風險報酬 : ", R)
 p = (R - d)/(u - d)
 
-s = [] # stock price
+
+### 計算 stock price tree
+s = [] 
 for i in range(n + 1) :
     s.append([])
     for j in range(i + 1) :
         SP = S * (u ** j) * (d ** (i-j))
         s[i].append(SP)
 
-prob = [[1.0], [p, round(1-p, 3)]] # probility
+
+### 計算 probilibity tree
+prob = [[1.0], [p, round(1-p, 3)]] 
 for i in range(2, n + 1) :
     prob.append([])
     for j in range(1) :
@@ -30,22 +34,46 @@ for i in range(2, n + 1) :
     for j in range(i, i+1) :
         prob[i].append(round((1-p) ** i, 3))
 
-c = [] # call price
+
+### 計算 call price tree
+c = [] 
 for i in range(n + 1) :
     c.append([])
     for j in range(i + 1) :
         if s[i][j] - X >= 0 :
             c[i].append(s[i][j] - X)
         else :
-            c[i].append(0)
+            c[i].append(0.0)
 
 for i in range(n-1, -1, -1) :
     for j in range(i + 1) :
         c[i][j] = round((p * c[i+1][j+1] + (1-p) * c[i+1][j])/R, 3)
 
-
-print(s)
-print(prob)
-print(c)
+c = pd.DataFrame(c)
+call_price = c.T
 
 
+### 計算 put price tree
+put = [] 
+for i in range(n + 1) :
+    put.append([])
+    for j in range(i + 1) :
+        if X - s[i][j] >= 0 :
+            put[i].append(X - s[i][j])
+        else :
+            put[i].append(0)
+
+for i in range(n-1, -1, -1) :
+    for j in range(i + 1) :
+        put[i][j] = round(((p) * put[i+1][j+1] + (1-p) * put[i+1][j])/R, 3)
+
+put = pd.DataFrame(put)
+put_price = put.T
+
+
+### print answer
+print("Call price table :\n\n", call_price, "\n")
+print("The call option price : ", call_price[0][0], "\n\n")
+
+print("Put price table :\n\n", put_price, "\n")
+print("The put option price : ", put_price[0][0])
